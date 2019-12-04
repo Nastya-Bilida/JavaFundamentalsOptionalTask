26cbaf1d0535023f8f10c7b9900fb9437c70c5f2
Nastya Bilida <abilida20@gmail.com>
2019-12-05 00:04:52 +0300

    fixed minor bugs

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 
-     static int getCountsOfDigits(long  number) {
+    static int getCountsOfDigits(long number) {
         int count = (number == 0) ? 1 : 0;
         while (number != 0) {
             count++;
@@ -12,60 +12,51 @@ public class Main {
         return count;
     }
 
-    static int getDifferentDigitsCount(long number){
-         
+    static int getDifferentDigitsCount(long number) {
         int differentDigitsCount = 0;
         int[] digitsCount = new int[10];
-        while (number!=0)
-        {
-            digitsCount[(int)(number%10)]++;
-            number/=10;
+        while (number != 0) {
+            digitsCount[(int) (number % 10)]++;
+            number /= 10;
         }
-        for (int i = 0; i < 10; i++)
-        {
+        for (int i = 0; i < 10; i++) {
             if (digitsCount[i] > 0) differentDigitsCount++;
         }
-
         return differentDigitsCount;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Add size array: ");
         int size = scanner.nextInt();
-        int[] array= new int[size];
-
+        int[] array = new int[size];
 
         System.out.println("Add array number:");
-        for(int i = 0; i < size; i++){
-             array[i] = scanner.nextInt();
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
         }
 
         int minNumber = array[0];
         int maxNumber = array[0];
-        int bigLength =  getCountsOfDigits(array[0]);
+        int bigLength = getCountsOfDigits(array[0]);
         int smallLength = getCountsOfDigits(array[0]);
         int countDifferentDigits = getDifferentDigitsCount(array[0]);
         int minNumberWithDifferentDigitsCount = array[0];
-       System.out.println("Your array number: " + Arrays.toString(array));
+        System.out.println("Your array number: " + Arrays.toString(array));
 
-       for(int i = 1; i < size; i++){
-         if (countDifferentDigits > getDifferentDigitsCount(array[i])) {
-             countDifferentDigits = getDifferentDigitsCount(array[i]);
-             minNumberWithDifferentDigitsCount = array[i];
-         }
-       }
+        for (int i = 1; i < size; i++) {
+            if (countDifferentDigits > getDifferentDigitsCount(array[i])) {
+                countDifferentDigits = getDifferentDigitsCount(array[i]);
+                minNumberWithDifferentDigitsCount = array[i];
+            }
+        }
         System.out.println("Minimum  number with different digits - " + minNumberWithDifferentDigitsCount + " count: " + countDifferentDigits);
 
-       for(int i = 1; i < size; i++) {
-
-           if (bigLength < getCountsOfDigits(array[i])) {
+        for (int i = 1; i < size; i++) {
+            if (bigLength < getCountsOfDigits(array[i])) {
                 bigLength = getCountsOfDigits(array[i]);
                 maxNumber = array[i];
-            }
-
-            else {
+            } else {
                 smallLength = getCountsOfDigits(array[i]);
                 minNumber = array[i];
             }
